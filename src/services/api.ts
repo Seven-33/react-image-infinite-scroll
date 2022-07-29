b34f2ef9110617d88896e7adf2f5d177bd457d61
robinsondev428 <princerobinson428withmail@gmail.com>
2022-07-29 02:48:44 -0700

    Updated api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,16 +1,14 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
 
 const unsplashApiKey = process.env.REACT_APP_UNSPLASH_API_KEY
-const baseURL = process.env.REACT_APP_UNSPLASH_API_KEY
+const baseURL = process.env.REACT_APP_UNSPLASH_BASE_URL
 
 const createAxiosInstance = (baseURL: string, time: number) => {
   const instance: AxiosInstance = axios.create({
     baseURL,
   })
   instance.interceptors.request.use((config: AxiosRequestConfig) => {
-    config.headers = {
-      Authorization: `Client-ID ${unsplashApiKey}`,
-    }
+    config.headers = { Authorization: `Client-ID ${unsplashApiKey}` }
     return config
   })
   return instance
@@ -18,8 +16,7 @@ const createAxiosInstance = (baseURL: string, time: number) => {
 
 const API = createAxiosInstance(baseURL ?? '', 30000)
 
-const fetchPhotos = () => {
-  API.get('/photos/random?count=15&orientation=portrait')
-}
+const fetchPhotos = (pageNumber: number) =>
+  API.get('/photos/?per_page=30&page=' + pageNumber)
 
 export { fetchPhotos }
